44bf6c0808bb61976b932a494af88f90a10378ea
RDeJonghe <rdejonghe@hotmail.com>
2023-03-01 14:00:27 -0500

    feat: simple route handling
    - added a routers directory for routers
    - uses express.Router
    - basic route added for a home page that sends basic info

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+import homeRouter from './routers/home';
 
+const app = express();
+app.use(express.json());
 app.use(cors());
+
+app.use('/', homeRouter);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening on ${PORT}`);
 });
+
