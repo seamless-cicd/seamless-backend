0592d5b26d5e47f065b65ba43d2aa43f6b1d90f4
RDeJonghe <rdejonghe@hotmail.com>
2023-03-01 19:44:36 -0500

    feat: route for runs data
    - get route for runs data
    - will return data for runs using prisma
    
    Co-authored-by: Jason Wang <jasonherngwang@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import homeRouter from './routers/home';
 import pipelinesRouter from './routers/pipelines';
 import servicesRouter from './routers/services';
+import runsRouter from './routers/runs';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cors());
 app.use('/', homeRouter);
 app.use('/pipelines', pipelinesRouter);
 app.use('/services', servicesRouter);
+app.use('/runs', runsRouter);
 
 const PORT = process.env.PORT || 3001;
 
