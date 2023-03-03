ba2bebc6d8381d3d930c7a1de9aedd773fee9da0
RDeJonghe <rdejonghe@hotmail.com>
2023-03-03 12:49:49 -0500

    chore: refactor query params
    - refactor route to use query params
    - in runs router

--- src/routers/runs.ts
+++ src/routers/runs.ts
@@ -5,17 +5,27 @@ const runsRouter = express.Router();
 
 // will get all runs for a particular service
 // useful for displaying all the runs that a service has
-runsRouter.get('/:serviceId', async (req: Request, res: Response) => {
-  const { serviceId } = req.params;
+// runsRouter.get('/:serviceId', async (req: Request, res: Response) => {
+//   const { serviceId } = req.params;
+//   const serviceData = await runsService.getAllForService(serviceId);
+//   res.status(200).json(serviceData);
+// });
+
+// refactoring above
+runsRouter.get('/', async (req: Request, res: Response) => {
+  const { serviceId } = req.query;
+  console.log(req.query);
   const serviceData = await runsService.getAllForService(serviceId);
   res.status(200).json(serviceData);
 });
 
-// retrieves data for the header with run info
-runsRouter.get('/header/:runId', async (req: Request, res: Response) => {
+// refactored to just /:runId to be more restful
+// retrieves data for a run - this is used for header info
+runsRouter.get('/:runId', async (req: Request, res: Response) => {
   const { runId } = req.params;
   const runData = await runsService.getOne(runId);
   res.status(200).json(runData);
 });
+
 
 export default runsRouter;
