2fd785ebeec402984bb984eea50f5bd52724be8e
RDeJonghe <rdejonghe@hotmail.com>
2023-03-07 20:03:10 -0500

    feat: add webhooks logic - logic added to webhooks router to distinguish between triggerOnMain, triggerOnPrOpen, triggerOnPrSync - logging info to show the difference - to do in future is add pipeline triggering code
    Co-authored-by: Jason Wang <jasonherngwang@gmail.com>

--- src/routers/webhooks.ts
+++ src/routers/webhooks.ts
@@ -1,13 +1,27 @@
-// router to handle webhooks
-
 import express, { Request, Response } from 'express';
 
 const webhooksRouter = express.Router();
 
 webhooksRouter.post('', async (req: Request, res: Response) => {
-  console.log(req.body);
+  const event = req.headers['x-github-event'];
+  const { ref, action } = req.body;
+  console.log(event);
+  console.log(ref);
+  console.log(action);
+  
+  if (event === 'push' && /\/main$/.test(ref)) {
+    console.log('push on main => trigger pipeline actions');
+  }
+  
+  if (event === 'pull_request' && action === 'opened') {
+    console.log('open pr => trigger pipeline');
+  }
+
+  if (event === 'pull_request' && action === 'synchronize') {
+    console.log('sync pr => trigger pipeline');
+  }
+  
   res.status(200).send();
-  // res.status(200).json({message:'webhook handled'});
 });
 
 export default webhooksRouter;
