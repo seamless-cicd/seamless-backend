b40bd22c006919b3d094b24462b66e34c30266b3
Jason Wang <jasonherngwang@gmail.com>
2023-03-07 16:04:00 -0800

    feat: start logs service

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// CAN ADD ENV VARIABLES IN FUTURE IF NEEDED
-// - prisma handles this on its own
+import { z } from 'zod';
 
-// const { DATABASE_URL } = process.env;
+const envSchema = z.object({
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.string().transform(Number),
+});
 
-// export { DATABASE_URL };
+export type EnvVars = z.infer<typeof envSchema>;
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables:',
+    JSON.stringify(parsedEnv.error.format()),
+  );
+  process.exit(1);
+}
+
+export const { REDIS_HOST, REDIS_PORT } = parsedEnv.data;
