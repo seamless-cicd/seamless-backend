67f0c0c4316ed2dad9019c16304c3893a0dd8c02
Ethan Weiner <ethanweiner20@gmail.com>
2023-03-13 12:18:15 -0400

    feat: switch oauth flow to use Octokit

--- src/routers/public/authenticate.ts
+++ src/routers/public/authenticate.ts
@@ -1,24 +1,34 @@
-import axios from 'axios';
+import { createOAuthUserAuth } from '@octokit/auth-oauth-user';
 import express, { Request, Response } from 'express';
 import { GITHUB_CLIENT_SECRET } from '../../utils/config';
-import { GITHUB_CLIENT_ID, GITHUB_OAUTH_URL } from '../../utils/constants';
+import { GITHUB_CLIENT_ID } from '../../utils/constants';
 
 const authRouter = express.Router();
 
 authRouter.get('/access-token', async (req: Request, res: Response) => {
-  // Supply the code from frontend so we can login on behalf of user
-  const { data } = await axios(
-    `${GITHUB_OAUTH_URL}?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${req.query.code}`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-      },
-    },
-  );
+  // Temporary code supplied to the frontend by GitHub OAuth that we can use to
+  // get an access token
+  const code = req.query.code;
+
+  if (!code || typeof code !== 'string' || code.length === 0) {
+    return res.status(400).send('No code provided');
+  }
+
+  const auth = createOAuthUserAuth({
+    clientId: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    code,
+  });
+
+  const { token } = await auth();
 
   // Pass access token back to frontend
-  return res.json(data);
+  return res.json({ token }).send(200);
+});
+
+// Test we can access user data
+authRouter.get('/user-data', async (req: Request, res: Response) => {
+  // Make an authorized request with OAuth
 });
 
 export default authRouter;
