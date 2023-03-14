23e0507ecde938af88fa27575092bda4bea1c612
Jason Wang <jasonherngwang@gmail.com>
2023-03-13 20:38:13 -0700

    feat: move redis middleware to single connection

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import express from 'express';
 
 import statusUpdatesRouter from './routers/private/status-updates';
 import homeRouter from './routers/public/home';
-import logsRouter from './routers/public/logs';
+import createLogsRouter from './routers/public/logs';
 import pipelinesRouter from './routers/public/pipelines';
 import runsRouter from './routers/public/runs';
 import servicesRouter from './routers/public/services';
 import stagesRouter from './routers/public/stages';
 import webhooksRouter from './routers/public/webhooks';
-import { redisMiddleware } from './utils/redis-middleware';
+import { redisClient } from './utils/redisClient';
 
 const app = express();
 app.use(express.json());
@@ -23,7 +23,7 @@ publicRouter.use('/pipelines', pipelinesRouter);
 publicRouter.use('/services', servicesRouter);
 publicRouter.use('/runs', runsRouter);
 publicRouter.use('/stages', stagesRouter);
-publicRouter.use('/logs', redisMiddleware, logsRouter);
+publicRouter.use('/logs', createLogsRouter(redisClient));
 publicRouter.use('/webhooks', webhooksRouter);
 
 app.use('/api', publicRouter);
@@ -33,13 +33,20 @@ const privateRouter = express.Router();
 privateRouter.use('/status-updates', statusUpdatesRouter);
 app.use('/internal', privateRouter);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.BACKEND_PORT || 3001;
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
 server.on('error', (error: Error) => {
-  // redisClient.quit();
+  redisClient.quit();
   console.error(`Failed to start server: ${error}`);
+});
+
+server.on('close', () => {
+  console.log('Server shutting down');
+  redisClient.quit(() => {
+    console.log('Redis client disconnected');
+  });
 });
