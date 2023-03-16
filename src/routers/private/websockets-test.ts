7eb74f6dc8bcc24d62af7840b42dd3dfcc180151
Ethan Weiner <ethanweiner20@gmail.com>
2023-03-16 10:42:47 -0400

    feat: add route for disconnecting from websockets

--- src/routers/private/websockets-test.ts
+++ src/routers/private/websockets-test.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
+import { deleteConnection } from '../../utils/websockets';
 
 const websocketsTestRouter = express.Router();
 
-const connectionIds = [];
+const connectionIds: string[] = [];
 
 // `/connect` route activates upon new connections to Websockets API
 websocketsTestRouter.post('/connect', async (req: Request, res: Response) => {
@@ -18,5 +19,23 @@ websocketsTestRouter.post('/connect', async (req: Request, res: Response) => {
 
   return res.sendStatus(200);
 });
+
+// `/disconnect` route activates when clients disconnect from Websockets API
+websocketsTestRouter.delete(
+  '/disconnect',
+  async (req: Request, res: Response) => {
+    const connectionId = req.body.connectionId;
+
+    if (!connectionId) {
+      throw new Error('No connection ID passed');
+    }
+
+    console.log(`Client with ${connectionId} disconnected from Websockets`);
+
+    deleteConnection(connectionIds, connectionId);
+
+    return res.sendStatus(200);
+  },
+);
 
 export default websocketsTestRouter;
