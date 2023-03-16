b3affcf00154f84a1284fedb3d02f11c691e6e67
Ethan Weiner <ethanweiner20@gmail.com>
2023-03-16 10:49:37 -0400

    feat: create temporary route for websockets testing

--- src/routers/private/websockets-test.ts
+++ src/routers/private/websockets-test.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} from '@aws-sdk/client-apigatewaymanagementapi';
 import express, { Request, Response } from 'express';
+import { WEBSOCKETS_API_URL } from '../../utils/config';
 import { deleteConnection } from '../../utils/websockets';
 
 const websocketsTestRouter = express.Router();
@@ -35,6 +41,48 @@ websocketsTestRouter.delete(
     deleteConnection(connectionIds, connectionId);
 
     return res.sendStatus(200);
+  },
+);
+
+// TEMPORARY: Route for posting messages to the client
+websocketsTestRouter.post(
+  '/send-message',
+  async (_: Request, res: Response) => {
+    const client = new ApiGatewayManagementApiClient({
+      endpoint: WEBSOCKETS_API_URL,
+      region: 'us-east-1',
+    });
+
+    const string = 'Hello World!';
+    const data = Uint8Array.from(string.split('').map((x) => x.charCodeAt(0)));
+
+    const postToConnectionCalls = connectionIds.map(async (connectionId) => {
+      try {
+        const requestParams = {
+          ConnectionId: connectionId,
+          Data: data,
+        };
+        const command = new PostToConnectionCommand(requestParams);
+        await client.send(command);
+      } catch (e) {
+        // @ts-ignore
+        if (e && e.statusCode === 410) {
+          console.log(`Found stale connection, deleting ${connectionId}`);
+          deleteConnection(connectionIds, connectionId);
+        } else {
+          throw e;
+        }
+      }
+    });
+
+    try {
+      await Promise.all(postToConnectionCalls);
+      console.log(`Messages to clients were sent.`);
+      res.sendStatus(204);
+    } catch (e) {
+      console.error(e);
+      res.sendStatus(500);
+    }
   },
 );
 
