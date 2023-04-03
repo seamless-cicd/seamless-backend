1ab101dac7f16292e4912af8ba3623706c42f3fa
Jason Wang <jasonherngwang@gmail.com>
2023-04-03 08:05:50 -0700

    fix: update gitignore to include ws url

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import stagesRouter from './routers/public/stages';
 import { userRouter } from './routers/public/user';
 import webhooksConfigRouter from './routers/public/webhook-config';
 import webhooksRouter from './routers/public/webhooks';
+import websocketsUrlRouter from './routers/public/websockets-url';
 
 import createLogUpdatesRouter from './routers/private/log-updates';
 import statusUpdatesRouter from './routers/private/status-updates';
@@ -45,6 +46,7 @@ publicRouter.use('/services', authMiddleware, servicesRouter);
 publicRouter.use('/runs', authMiddleware, runsRouter);
 publicRouter.use('/stages', authMiddleware, stagesRouter);
 publicRouter.use('/user', authMiddleware, userRouter);
+publicRouter.use('/websockets-url', authMiddleware, websocketsUrlRouter);
 
 app.use('/api', publicRouter);
 
